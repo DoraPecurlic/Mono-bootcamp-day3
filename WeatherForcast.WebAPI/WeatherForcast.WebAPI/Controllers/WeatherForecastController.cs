f83e37c1ff52895979a6b329afc4a5ceaf438a61
DoraPecurlic <dora.pecurlic01@gmail.com>
2024-05-29 18:02:23 +0200

    modified generating forcasts and get method

--- WeatherForcast.WebAPI/WeatherForcast.WebAPI/Controllers/WeatherForecastController.cs
+++ WeatherForcast.WebAPI/WeatherForcast.WebAPI/Controllers/WeatherForecastController.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System;
 
 namespace WeatherForcast.WebAPI.Controllers
 {
@@ -11,6 +13,36 @@ namespace WeatherForcast.WebAPI.Controllers
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
+        private static List<WeatherForecast> forecasts = new List<WeatherForecast>();
+
+        [HttpPost("GenerateWeatherForecast", Name ="GenerateWeatherForecast")]
+        public IActionResult GenerateWeatherForecast()
+        {
+            for (int i = 0; i < 10; i++) 
+            {
+               DateOnly date = DateOnly.FromDateTime(DateTime.Now.AddDays(i));
+               int temperatureC = Random.Shared.Next(-20, 55);
+               string summary = Summaries[Random.Shared.Next(Summaries.Count)];
+
+                forecasts.Add(new WeatherForecast(date, temperatureC, summary));
+            }
+
+            return Ok("Weather forcasts generated successffully.");
+            
+        }
+
+        [HttpGet(Name = "GetWeatherForecasts")]
+        public IActionResult GetWeatherForecasts()
+        {
+            List<string> weatherForcasts = new List<string>();
+            foreach(var forecast in forecasts)
+            {
+                weatherForcasts.Add($"Date:{forecast.Date}, Temperature: {forecast.TemperatureC}, Summary: {forecast.Summary}");
+            }
+            return Ok(weatherForcasts.ToArray());
+        }
+
+
         private readonly ILogger<WeatherForecastController> _logger;
 
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
@@ -18,20 +50,11 @@ namespace WeatherForcast.WebAPI.Controllers
             _logger = logger;
         }
 
-        [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
-        {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Count)]
-            })
-            .ToArray();
-        }
+        
 
 
-        [HttpPost(Name = "InsertSummary")]
+
+        [HttpPost("InsertSummary", Name = "InsertSummary")]
         public IActionResult InsertSummary(string summary)
         {
             if (Summaries.Contains(summary))
@@ -57,6 +80,9 @@ namespace WeatherForcast.WebAPI.Controllers
             Summaries.RemoveAt(Summaries.IndexOf(summary));
             return StatusCode(200, "Summary successfully deleted.");
         }
+
+
+       
 
     }
 }
